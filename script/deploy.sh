967e0f305148ac93c64c9ad2e152a5085fde11d3
Petro Podrezo <petro@podrezo.com>
2019-12-15 20:50:51 -0500

    Fix deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-TMP_FILE=/tmp/toronto_schedule_scraper.zip
+tmp_dir=$(mktemp -d -t toronto_schedule_scraper-XXXXXXXXXX)
+echo $tmp_dir
+cp -r .bundle index.rb schedule_parser.rb Gemfile Gemfile.lock $tmp_dir
+zip_file=$(mktemp -t toronto_schedule_scraper-XXXXXXXXXX.zip --dry-run)
 
-rm "$TMP_FILE"
-docker run -it --rm -v "$PWD":/var/task lambci/lambda:build-ruby2.5 bundle install
-zip "$TMP_FILE" -r .
+docker run -it --rm -v "$tmp_dir":/var/task lambci/lambda:build-ruby2.5 bundle install --without=development
+pushd $tmp_dir
+zip $zip_file -r .
+popd
 aws lambda update-function-code \
     --function-name arn:aws:lambda:us-east-1:234147186957:function:TorontoScheduleScraper \
-    --zip-file "fileb://$TMP_FILE" \
+    --zip-file "fileb://$zip_file" \
     --region us-east-1
